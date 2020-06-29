8bf3476bde9963b0136b4bda0ad57f15a7ca557a
Leo Köberlein <leo@wolfgang-koeberlein.de>
2020-06-29 15:33:30 +0200

    Implemented JOINs on foreignKey matching

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,18 +1,11 @@
 (function() {
 
 
-    let identifiable = {
-        identifier: '',
-        toString: function() {
-            return this.identifier;
-        }
-    }
-
     //removes all whitespace
     let clean = x => x.replace(/\s+/g, '');
 
     function parseAttribute(description) {
-        let attribute = Object.create(identifiable);
+        let attribute = Object.create(null);
         let firstBracket = description.indexOf('[');
         if (firstBracket >= 0) {
             let lastBracket = description.lastIndexOf(']');
@@ -26,7 +19,7 @@
             attribute.keyCandidate = true;
             description = description.substring(1, description.length - 1);
         }
-        attribute.identifier = description;
+        attribute.name = description;
         return attribute;
     }
 
@@ -37,11 +30,11 @@
         let lastBracket = description.lastIndexOf(')');
         if (lastBracket < 0) throw Error(`Missing closing bracket in line:\n${description}`);
 
-        let table = Object.create(identifiable);
+        let table = Object.create(null);
         table.attributes = description.substring(firstBracket + 1, lastBracket).split(',').map(parseAttribute);
         table.primaryKey = table.attributes.filter(x => x.keyCandidate)[0];
         if (!table.primaryKey) throw Error(`Missing primaryKey for table at:\n${description}`);
-        table.identifier = description.substring(0, firstBracket);
+        table.name = description.substring(0, firstBracket);
         return table;
     }
 
@@ -50,9 +43,9 @@
         schema.forEach(table => table.attributes.forEach(attribute => {
             if (attribute.referencesTable) {
                 attribute.referencesTable =
-                    schema.filter(table2 => table2.identifier == attribute.referencesTable)[0];
+                    schema.filter(table2 => table2.name == attribute.referencesTable)[0];
                 if (!attribute.referencesTable)
-                    throw Error(`Table referenced by ${table.identifier}.${attribute.identifier}` +
+                    throw Error(`Table referenced by ${table.name}.${attribute.name}` +
                         ' not found.');
             }
         }));
@@ -61,43 +54,66 @@
 
 
     let querySpecification = {
-        select: ['*'],
-        from: [],
-        where: null,
-        gropBy: null,
-        having: null,
-        orderBy: null,
-        toString: function() {
+        select: ['*']
+    }
+
+    let JOIN_TYPES = ['JOIN', 'LEFT JOIN', 'RIGHT JOIN', 'FULL OUTER JOIN'];
+
+    function RandomSelectQuery(schema, options) {
+        let availableAttributes = [];
+
+        this.from = [];
+        let aliases = Object.create(null);
+        schema.forEach(table => aliases[table.name] = 1);
+        let numTables =
+            Math.floor(Math.random() * (options.maxTables + 1 - options.minTables)) + options.minTables;
+        for (let i = 0; i < numTables; ++i) {
+            let tableRef = {
+                table: schema[Math.floor(Math.random() * schema.length)]
+            };
+            tableRef.alias = tableRef.table.name.toString().substr(0, 2);
+            if (aliases[tableRef.alias]) tableRef.alias += ++aliases[tableRef.alias];
+            else aliases[tableRef.alias] = 1;
+
+            if (i > 0) {
+                let referencePairs = [];
+                tableRef.table.attributes.forEach(x => {
+                    if (x.referencesTable)
+                        availableAttributes.forEach(y => {
+                            if (x.referencesTable == y.tableRef.table && y.tableRef.table.primaryKey == y.attribute)
+                                referencePairs.push({
+                                    x: x,
+                                    y: y
+                                });
+                        })
+                });
+                if (referencePairs.length > 0 && Math.random() < 0.8) {
+                    tableRef.join = JOIN_TYPES[Math.floor(Math.random() * JOIN_TYPES.length)];
+                    let refPair = referencePairs[Math.floor(Math.random() * referencePairs.length)];
+                    tableRef.on = `${tableRef.alias}.${refPair.x.name} = ${refPair.y.tableRef.alias}.${refPair.y.attribute.name}`
+                }
+            }
+
+            this.from.push(tableRef);
+            tableRef.table.attributes.forEach(x => availableAttributes.push({
+                attribute: x,
+                tableRef: tableRef
+            }));
+        }
+
+
+
+        this.select = ['*'];
+
+        this.toString = function() {
+            console.log(this.from);
             return 'SELECT ' + this.select.join(', ') + '\n' +
-                'FROM ' + this.from.join(', \n\t') + '\n' +
+                'FROM ' + this.from.reduce(((x, y) => (x ? x + (y.join ? `\n\t${y.join} ` : ', \n\t') : '') + y.table.name + ' ' + y.alias + (y.on ? ` ON (${y.on})` : '')), null) + '\n' +
                 (this.where ? 'WHERE ' + this.whereWhere + '\n' : '') +
                 (this.groupBy ? 'GROUP BY ' + this.groupBy.join(', ') + '\n' : '') +
                 (this.havingClause ? 'HAVING ' + this.havingClause + '\n' : '') +
                 (this.orderBy ? 'ORDER BY ' + this.orderBy + '\n' : '');
         }
-    }
-
-    function generateSelectQuery(schema, options) {
-        let query = Object.create(querySpecification);
-
-        query.availableAttributes = [];
-        query.from = [];
-        query.aliases = Object.create(null);
-        schema.forEach(table => query.aliases[table.identifier] = 1);
-        let numTables =
-            Math.floor(Math.random() * (options.maxTables + 1 - options.minTables)) + options.minTables;
-        for (let i = 0; i < numTables; ++i) {
-            let table = Object.create(schema[Math.floor(Math.random() * schema.length)]);
-            table.alias = table.toString().substr(0, 2);
-            if (query.aliases[table.alias]) table.alias += ++query.aliases[table.alias];
-            else query.aliases[table.alias] = 1;
-            table.toString = function() {
-                return this.identifier + ' ' + this.alias
-            };
-            query.from.push(table);
-        }
-
-        return query;
     }
 
 
@@ -114,13 +130,13 @@
     }
 
     window.generateSelect = function() {
-        try {
-            let schema = parseSchema(selectSchema.value);
-            let options = fetchSelectOptions();
-            let query = generateSelectQuery(schema, options);
-            selectResult.value = query;
-        } catch (e) {
-            alert(e);
-        }
+        //try {
+        let schema = parseSchema(selectSchema.value);
+        let options = fetchSelectOptions();
+        let query = new RandomSelectQuery(schema, options);
+        selectResult.value = query;
+        // } catch (e) {
+        //     alert(e);
+        // }
     }
 })();
