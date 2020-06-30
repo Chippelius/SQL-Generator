c68a1f9fe8a30b0a878eff66ae74b4033015e0d5
Leo Köberlein <leo@wolfgang-koeberlein.de>
2020-06-30 12:23:16 +0200

    Implemented advanced SELECT

--- docs/js/index.js
+++ docs/js/index.js
@@ -55,101 +55,148 @@
 
     let JOIN_TYPES = ['JOIN', 'LEFT JOIN', 'RIGHT JOIN', 'FULL OUTER JOIN'];
 
-	function randomNumericExpression(attrRefs) {
-		if(!attrRefs || attrRefs.length < 1) {
-			return Math.floor(Math.random()*100)/10;
-		} else if(Math.random() < 0.7) {
-			let attr = attrRefs[Math.floor(Math.random()*attrRefs.length)];
-			return `${attr.tableRef.alias}.${attr.attribute.name}`;
-		} else if(Math.random() < 0.7) {
-			return '('+randomNumericExpression(attrRefs) + ') * ('
-				+ randomNumericExpression(attrRefs.filter(x=>Math.random()<0.1))+')';
-		} else {
-			return '('+randomNumericExpression(attrRefs) + ') + ('
-				+ randomNumericExpression(attrRefs.filter(x=>Math.random()<0.1))+')';
-		}
-	}
-	window.randomNumericExpression = randomNumericExpression;
+    function randomNumericExpression(attributes) {
+        if (!attributes || attributes.length < 1) {
+            return Math.floor(Math.random() * 100) / 10;
+        } else if (Math.random() < 0.8) {
+            let attribute = attributes[Math.floor(Math.random() * attributes.length)];
+            return `${attribute}`;
+        } else if (Math.random() < 0.7) {
+            return '(' + randomNumericExpression(attributes) + ') * (' +
+                randomNumericExpression(attributes.filter(x => Math.random() < 0.1)) + ')';
+        } else {
+            return '(' + randomNumericExpression(attributes) + ') + (' +
+                randomNumericExpression(attributes.filter(x => Math.random() < 0.1)) + ')';
+        }
+    }
 
-	function randomBooleanExpression(attrRefs) {
-		switch(Math.floor(Math.random()*12)) {
-		case 0:
-		case 1:
-			return 'NOT ('+randomBooleanExpression(attrRefs)+')';
-		case 2:
-		case 3:
-			return '('+randomBooleanExpression(attrRefs)+') AND ('+randomBooleanExpression(attrRefs)+')';
-		case 4:
-		case 5:
-			return '('+randomBooleanExpression(attrRefs)+') OR ('+randomBooleanExpression(attrRefs)+')';
-		case 6:
-			return randomNumericExpression(attrRefs)+' < '+randomNumericExpression(null);
-		case 7:
-			return randomNumericExpression(attrRefs)+' > '+randomNumericExpression(null);
-		case 8:
-			return randomNumericExpression(attrRefs)+' <= '+randomNumericExpression(null);
-		case 9:
-			return randomNumericExpression(attrRefs)+' >= '+randomNumericExpression(null);
-		default:
-			return randomNumericExpression(attrRefs)+' = '+randomNumericExpression(null);
-		}
-	}
-	window.randomBooleanExpression = randomBooleanExpression;
+    function randomBooleanExpression(attributes) {
+        switch (Math.floor(Math.random() * 13)) {
+            case 0:
+            case 1:
+                return randomNumericExpression(attributes) + ' = ' + randomNumericExpression(attributes);
+            case 2:
+                return randomNumericExpression(attributes) + ' < ' + randomNumericExpression(attributes);
+            case 3:
+                return randomNumericExpression(attributes) + ' > ' + randomNumericExpression(attributes);
+            case 4:
+                return randomNumericExpression(attributes) + ' <= ' + randomNumericExpression(attributes);
+            case 5:
+                return randomNumericExpression(attributes) + ' >= ' + randomNumericExpression(attributes);
+            case 6:
+            case 7:
+                return 'NOT (' + randomBooleanExpression(attributes) + ')';
+            case 8:
+            case 9:
+                return '(' + randomBooleanExpression(attributes) + ') AND (' + randomBooleanExpression(attributes) + ')';
+            case 10:
+            case 11:
+                return '(' + randomBooleanExpression(attributes) + ') OR (' + randomBooleanExpression(attributes) + ')';
+            case 12:
+                let attribute = attributes[Math.floor(Math.random() * attributes.length)];
+                return `${attribute} IS NULL`;
+        }
+    }
+
+    function randomAggregation(attribute) {
+        switch (Math.floor(Math.random() * 5)) {
+            case 0:
+                return `MIN(${attribute})`;
+            case 1:
+                return `MAX(${attribute})`;
+            case 2:
+                return `AVG(${attribute})`;
+            case 3:
+                return `SUM(${attribute})`;
+            default:
+                return `COUNT(${attribute})`;
+        }
+    }
 
     function RandomSelectQuery(schema, options) {
         let availableAttributes = [];
 
-		//=== FROM ===
+        //=== FROM ===
         this.from = [];
         let aliases = Object.create(null);
         schema.forEach(table => aliases[table.name] = 1);
         let numTables =
             Math.floor(Math.random() * (options.maxTables + 1 - options.minTables)) + options.minTables;
         for (let i = 0; i < numTables; ++i) {
-            let tableRef = {
-                table: schema[Math.floor(Math.random() * schema.length)]
-            };
-            tableRef.alias = tableRef.table.name.toString().substr(0, 2);
-            if (aliases[tableRef.alias]) tableRef.alias += ++aliases[tableRef.alias];
-            else aliases[tableRef.alias] = 1;
-			// JOIN
+            table = schema[Math.floor(Math.random() * schema.length)];
+            let alias = table.name.toString().substr(0, 2);
+            if (aliases[alias]) alias += ++aliases[alias];
+            else aliases[alias] = 1;
+            // JOIN
+            let join = null,
+                on = null;
             if (i > 0) { //only allow JOINs starting at the second table
-                let referencePairs = [];
-                tableRef.table.attributes.forEach(x => {
-                    if (x.referencesTable)
-                        availableAttributes.forEach(y => {
-                            if (x.referencesTable == y.tableRef.table && y.tableRef.table.primaryKey == y.attribute)
-                                referencePairs.push({
-                                    x: x,
-                                    y: y
-                                });
-                        })
-                });
-                if (referencePairs.length > 0 && Math.random() < 0.8) {
-                    tableRef.join = JOIN_TYPES[Math.floor(Math.random() * JOIN_TYPES.length)];
-                    let refPair = referencePairs[Math.floor(Math.random() * referencePairs.length)];
-                    tableRef.on = `${tableRef.alias}.${refPair.x.name} = ${refPair.y.tableRef.alias}.${refPair.y.attribute.name}`
+                let references = availableAttributes.filter(x => x.referencesTable == table);
+                if (references.length > 0 && Math.random() < 0.8) {
+                    join = JOIN_TYPES[Math.floor(Math.random() * JOIN_TYPES.length)];
+                    let ref = references[Math.floor(Math.random() * references.length)];
+                    on = `${ref.name} = ${alias}.${table.primaryKey.name}`;
                 }
             }
-            this.from.push(tableRef);
-            tableRef.table.attributes.forEach(x => availableAttributes.push({
-                attribute: x,
-                tableRef: tableRef
+            this.from.push({
+                name: table.name,
+                alias: alias,
+                join: join,
+                on: on
+            });
+            table.attributes.forEach(x => availableAttributes.push({
+                name: `${alias}.${x.name}`,
+                referencesTable: x.referencesTable,
+                keyCandidate: x.keyCandidate
             }));
         }
 
-		//=== WHERE ===
-		this.where = randomBooleanExpression(availableAttributes.filter(x => !x.attribute.referencesTable));
+        //=== WHERE ===
+        if (Math.random() < 0.5)
+            this.where = randomBooleanExpression(availableAttributes.filter(x => !x.referencesTable && !x.keyCandidate).map(x => x.name));
 
-		//=== SELECT ===
-		this.select = availableAttributes.filter(x => Math.random() < 0.3);
-		if(this.select.length < 1 || Math.random() < 0.2) {
-        	this.select = availableAttributes;
-		}
+        //=== SELECT ===
+        this.select = [];
+        if (Math.random() < 0.8) {
+            let numericAttributes = availableAttributes.filter(x => !x.referencesTable && !x.keyCandidate);
+            let numColumns = Math.floor(Math.random() * (options.maxColumns + 1 - options.minColumns)) + options.minColumns;
+            for (let i = 0; i < numColumns; ++i) {
+                let attribute = availableAttributes[Math.floor(Math.random() * availableAttributes.length)];
+                let column = attribute.name;
+                if (!attribute.referencesTable && !attribute.keyCandidate) {
+                    if (Math.random() < 0.1) {
+                        column = randomAggregation(column);
+                    } else if (Math.random() < 0.1) {
+                        column = randomAggregation(randomNumericExpression([column]));
+                    }
+                    if (Math.random() < 0.3) {
+                        let attribute2 = numericAttributes[Math.floor(Math.random() * numericAttributes.length)];
+                        if (Math.random() < 0.5) {
+                            column = randomNumericExpression([column, attribute2.name]);
+                        } else {
+                            column = randomAggregation(randomNumericExpression([column, attribute2.name]));
+                        }
+                    }
+                    if (Math.random() < 0.1) {
+                        column = randomNumericExpression([column]);
+                    } else if (Math.random() < 0.1) {
+                        column = randomBooleanExpression([column]);
+                    } else if (Math.random() < 0.3) {
+                        let attribute2 = numericAttributes[Math.floor(Math.random() * numericAttributes.length)];
+                        if (Math.random() < 0.9) {
+                            column = randomNumericExpression([column, attribute2.name]);
+                        } else {
+                            column = randomBooleanExpression([column, attribute2.name]);
+                        }
+                    }
+                }
+                this.select.push(column);
+            }
+        }
 
         this.toString = function() {
-            return 'SELECT ' + (this.select.length < 1 || this.select.length==availableAttributes.length?'*':this.select.reduce(((x,y) => (x?x+', ':'')+`${y.tableRef.alias}.${y.attribute.name}`), null)) + '\n' +
-                'FROM ' + this.from.reduce(((x, y) => (x ? x + (y.join ? `\n\t${y.join} ` : ', \n\t') : '') + y.table.name + ' ' + y.alias + (y.on ? ` ON (${y.on})` : '')), null) + '\n' +
+            return 'SELECT ' + (this.select.length < 1 ? '*' : this.select.join(', ')) + '\n' +
+                'FROM ' + this.from.reduce(((x, y) => (x ? x + (y.join ? `\n\t${y.join} ` : ', \n\t') : '') + y.name + ' ' + y.alias + (y.on ? ` ON (${y.on})` : '')), null) + '\n' +
                 (this.where ? 'WHERE ' + this.where + '\n' : '') +
                 (this.groupBy ? 'GROUP BY ' + this.groupBy.join(', ') + '\n' : '') +
                 (this.havingClause ? 'HAVING ' + this.havingClause + '\n' : '') +
@@ -167,17 +214,19 @@
         let options = Object.create(null);
         options.minTables = 1;
         options.maxTables = 5;
+        options.minColumns = 2;
+        options.maxColumns = 8;
         return options;
     }
 
     window.generateSelect = function() {
-        //try {
-        let schema = parseSchema(selectSchema.value);
-        let options = fetchSelectOptions();
-        let query = new RandomSelectQuery(schema, options);
-        selectResult.value = query;
-        // } catch (e) {
-        //     alert(e);
-        // }
+        try {
+            let schema = parseSchema(selectSchema.value);
+            let options = fetchSelectOptions();
+            let query = new RandomSelectQuery(schema, options);
+            selectResult.value = query;
+        } catch (e) {
+            alert(e);
+        }
     }
 })();
